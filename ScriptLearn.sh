85f6d1f170a65b9164ed05db490b5b7cc3c96997
G. S. Varma <17321286+gsvarma@users.noreply.github.com>
2018-01-28 18:06:42 +0100

    Fixes bad substitution in ScriptLearn.

--- ScriptLearn.sh
+++ ScriptLearn.sh
@@ -5,6 +5,6 @@ export MAVEN_OPTS="-Xmx6G -Dlog4j.configuration=file:data/fox/log4j.properties"
 ARGS="-len -atrain -iinput/Wikiner/aij-wikiner-it-wp3.bz2"
 
 
-#nohup mvn exec:java -Dexec.mainClass="org.aksw.fox.ui.FoxCLI" -Dexec.args="$ARGS" > logLearn.log &
+nohup mvn exec:java -Dexec.mainClass="org.aksw.fox.ui.FoxCLI" -Dexec.args="$ARGS" > logLearn.log &
 
-nohup java -Xmx6G -cp fox-${project.version}-jar-with-dependencies.jar org.aksw.fox.FoxCLI -lit -atrain -iinput/Wikiner/aij-wikiner-it-wp3.bz2 > learn.log &
+#nohup java -Xmx6G -cp fox-${project.version}-jar-with-dependencies.jar org.aksw.fox.FoxCLI -lit -atrain -iinput/Wikiner/aij-wikiner-it-wp3.bz2 > learn.log &
--- ScriptValidate.sh
+++ ScriptValidate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export MAVEN_OPTS="-Xmx6G -Dlog4j.configuration=file:data/fox/log4j.properties"
 
-nohup mvn -e exec:java -Dexec.mainClass="org.aksw.fox.ui.FoxCLI" -Dexec.args="-lit -avalidate -iinput/Wikiner/aij-wikiner-it-wp3.bz2" > validate.log &
+nohup mvn -X exec:java -Dexec.mainClass="org.aksw.fox.ui.FoxCLI" -Dexec.args="-lit -avalidate -iinput/Wikiner/aij-wikiner-it-wp3.bz2" > validate.log &
 
